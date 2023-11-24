7847beb0300f8692a3f071238ef477de881a892f
Maryana Varfolomeeva <mihralievam@icloud.com>
2023-11-25 00:54:46 +0500

    update unit-tests

--- src/test/java/ru/netology/CallbackTest.java
+++ src/test/java/ru/netology/CallbackTest.java
@@ -1,10 +1,11 @@
 package ru.netology;
 
 import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.conditions.Visible;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.cssValue;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -14,62 +15,77 @@ public class CallbackTest {
     @BeforeEach
     void setUp() {
         open("http://0.0.0.0:9999");
-        SelenideElement form = $("[data-test-id=\"name\"]");
-
     }
 
     @Test
     void shouldSubmitRequest() {
-        $("[data-test-id=\"name\"] input").setValue("Оксана Иванова");
-        $("[data-test-id=\"phone\"] input").setValue("+79128114397");
-        $("[data-test-id=\"agreement\"]").click();
+
+        $("[data-test-id=name] input").setValue("Василий Озеров");
+        $("[data-test-id=phone] input").setValue("+79270000000");
+        $("[data-test-id=agreement]").click();
         $("button").click();
-        String text = $("[data-test-id=\"order-success\"]").getText();
-        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+        $("[data-test-id=order-success]").shouldBe(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 
     @Test
     void shouldNotifyAboutInvalidName() {
-        $("[data-test-id=\"name\"] input").setValue("Oxana Ivanova");
+        $("[data-test-id=name] input").setValue("Vasili Ozerov");
+        $("[data-test-id=phone] input").setValue("+79270000000");
+        $("[data-test-id=agreement]").click();
         $("button").click();
-        String text = $("[data-test-id=\"name\"]").$("[class=\"input__sub\"]").getText();
-        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text.trim());
+        $("[data-test-id=name]").$(".input__sub").shouldBe(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void shouldNotifyAboutInvalidName2() {
+        $("[data-test-id=name] input").setValue("Василий Озеров!");
+        $("[data-test-id=phone] input").setValue("+79270000000");
+        $("[data-test-id=agreement]").click();
+        $("button").click();
+        $("[data-test-id=name]").$(".input__sub").shouldBe(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
 
     @Test
     void shouldNotifyAboutEmptyName() {
-
-        $("[data-test-id=\"phone\"] input").setValue("+79128114397");
-        $("[data-test-id=\"agreement\"]").click();
+        $("[data-test-id=name] input").setValue("");
+        $("[data-test-id=phone] input").setValue("+79270000000");
+        $("[data-test-id=agreement]").click();
         $("button").click();
-        String text = $("[data-test-id=\"name\"]").$("[class=\"input__sub\"]").getText();
-        assertEquals("Поле обязательно для заполнения", text.trim());
+        $("[data-test-id=name]").$(".input__sub").shouldBe(exactText("Поле обязательно для заполнения"));
     }
 
     @Test
     void shouldNotifyAboutInvalidPhone() {
-        $("[data-test-id=\"name\"] input").setValue("Оксана Иванова");
-        $("[data-test-id=\"phone\"] input").setValue("+791281143");
+        $("[data-test-id=name] input").setValue("Василий Озеров");
+        $("[data-test-id=phone] input").setValue("+7927000000");
+        $("[data-test-id=agreement]").click();
         $("button").click();
-        String text = $("[data-test-id=\"phone\"]").$("[class=\"input__sub\"]").getText();
-        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", text.trim());
+        $("[data-test-id=phone]").$(".input__sub").shouldBe(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    void shouldNotifyAboutInvalidPhone2() {
+        $("[data-test-id=name] input").setValue("Василий Озеров");
+        $("[data-test-id=phone] input").setValue("89270000000");
+        $("[data-test-id=agreement]").click();
+        $("button").click();
+        $("[data-test-id=phone]").$(".input__sub").shouldBe(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
 
     @Test
     void shouldNotifyAboutEmptyPhone() {
-        $("[data-test-id=\"name\"] input").setValue("Оксана Иванова");
+        $("[data-test-id=name] input").setValue("Василий Озеров");
+        $("[data-test-id=phone] input").setValue("");
+        $("[data-test-id=agreement]").click();
         $("button").click();
-        String text = $("[data-test-id=\"phone\"]").$("[class=\"input__sub\"]").getText();
-        assertEquals("Поле обязательно для заполнения", text.trim());
+        $("[data-test-id=phone]").$(".input__sub").shouldBe(exactText("Поле обязательно для заполнения"));
     }
 
     @Test
-    void shouldRedColorForUncheckedAgreementText() {
-        $("[data-test-id=\"name\"] input").setValue("Оксана Иванова");
-        $("[data-test-id=\"phone\"] input").setValue("+79128114397");
+    void shouldNotifyAboutUncheckedAgreement() {
+        $("[data-test-id=name] input").setValue("Василий Озеров");
+        $("[data-test-id=phone] input").setValue("+79270000000");
         $("button").click();
-        String text = $("[data-test-id=\"agreement\"]").$("[class=\"checkbox__text\"]").getCssValue("color");
-        assertEquals("rgba(255, 92, 92, 1)", text.trim());
+        $(".input_invalid").shouldBe(visible);
     }
-
 }
