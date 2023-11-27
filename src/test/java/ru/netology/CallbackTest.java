943465c0ce7d4e4d909867e1a50b85055d25250a
Maryana Varfolomeeva <mihralievam@icloud.com>
2023-11-27 20:05:47 +0500

    исправить комб. селекторы

--- src/test/java/ru/netology/CallbackTest.java
+++ src/test/java/ru/netology/CallbackTest.java
@@ -33,7 +33,7 @@ public class CallbackTest {
         $("[data-test-id=phone] input").setValue("+79270000000");
         $("[data-test-id=agreement]").click();
         $("button").click();
-        $("[data-test-id=name].input_invalid").shouldBe(visible).$(".input__sub").shouldBe(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $("[data-test-id='name'].input_invalid .input__sub").shouldBe(visible, exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
 
     @Test
@@ -42,7 +42,7 @@ public class CallbackTest {
         $("[data-test-id=phone] input").setValue("+79270000000");
         $("[data-test-id=agreement]").click();
         $("button").click();
-        $("[data-test-id=name].input_invalid").shouldBe(visible).$(".input__sub").shouldBe(exactText("Поле обязательно для заполнения"));
+        $("[data-test-id=name].input_invalid .input__sub").shouldBe(visible, exactText("Поле обязательно для заполнения"));
     }
 
     @Test
@@ -51,7 +51,7 @@ public class CallbackTest {
         $("[data-test-id=phone] input").setValue("+7927000000");
         $("[data-test-id=agreement]").click();
         $("button").click();
-        $("[data-test-id=phone].input_invalid").shouldBe(visible).$(".input__sub").shouldBe(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+        $("[data-test-id=phone].input_invalid .input__sub").shouldBe(visible, exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
 
     @Test
@@ -60,7 +60,7 @@ public class CallbackTest {
         $("[data-test-id=phone] input").setValue("");
         $("[data-test-id=agreement]").click();
         $("button").click();
-        $("[data-test-id=phone].input_invalid").shouldBe(visible).$(".input__sub").shouldBe(exactText("Поле обязательно для заполнения"));
+        $("[data-test-id=phone].input_invalid .input__sub").shouldBe(visible, exactText("Поле обязательно для заполнения"));
     }
 
     @Test
